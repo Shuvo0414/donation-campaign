cfabafe14e82be8840717895456b1f7b114c3b1d
Shuvo0414 <138393384+Shuvo0414@users.noreply.github.com>
2023-09-25 08:38:56 +0600

    feat: Display stored donations or a message when no donations found - Implemented logic to retrieve stored donation data from localStorage - If there are no donations, it encourages to contribute with a user-friendly message

--- src/pages/Donation/Donation.jsx
+++ src/pages/Donation/Donation.jsx
@@ -1,7 +1,33 @@
+import { useEffect, useState } from "react";
+
 const Donation = () => {
+  const [donation, setDonation] = useState([]);
+  const [noDataFound, setNoDataFound] = useState("");
+
+  useEffect(() => {
+    const getDonationDataFormLs = JSON.parse(localStorage.getItem("donations"));
+    if (getDonationDataFormLs) {
+      setDonation(getDonationDataFormLs);
+    } else {
+      setNoDataFound(
+        "There are no donations at the moment. Your contribution can change that!"
+      );
+    }
+  }, []);
+  console.log(noDataFound);
+  console.log(donation);
+
   return (
     <div>
-      <h2>Donation</h2>
+      {noDataFound ? (
+        <p className=" text-center mt-60  text-lg font-semibold lg:text-xl p-1 lg:p-0 ">
+          {noDataFound}
+        </p>
+      ) : (
+        <div>
+          <h1>thanks for donation</h1>
+        </div>
+      )}
     </div>
   );
 };
