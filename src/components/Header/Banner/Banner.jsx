1a78cc08a3e7681d441b21b9f18f752bfc301034
Shuvo0414 <138393384+Shuvo0414@users.noreply.github.com>
2023-09-25 19:41:41 +0600

    feat: Implement search functionality in Home component - Send function and string in the Banner component - Implemented filtering of donations based on search category - Cleared input field after a successful search

--- src/components/Header/Banner/Banner.jsx
+++ src/components/Header/Banner/Banner.jsx
@@ -1,3 +1,5 @@
+import PropTypes from "prop-types";
+
 import bannerBg from "../../../assets/FB_IMG_1695618253156.jpg";
 
 const bannerStyle = {
@@ -7,7 +9,14 @@ const bannerStyle = {
   backgroundColor: "rgba(255, 255, 255, 0.95)",
 };
 
-const Banner = () => {
+const Banner = ({
+  searchCategory,
+  handleCategoryChange,
+  handleSearchClick,
+}) => {
+  // console.log(handleSearchClick);
+  // console.log(searchCategory);
+  // console.log(handleCategoryChange);
   return (
     <div
       className=" bg-blend-overlay   w-full h-full md:w-[80%] md:h-[60vh] container mx-auto px-8 md:px-10 lg:px-24 p-5 my-10 md:mt-36 "
@@ -22,14 +31,25 @@ const Banner = () => {
             className=" w-[230px] md:w-[470px] px-2 md:px-4 py-2 md:py-4 rounded-lg border border-red-200  text-[#0B0B0B66]"
             type="text"
             placeholder="Search here...."
+            value={searchCategory}
+            onChange={handleCategoryChange}
           />
-          <button className=" py-2 px-3 md:py-4 md:px-7 bg-[#FF444A] rounded-lg text-white ml-2">
+          <button
+            onClick={handleSearchClick}
+            className=" py-2 px-3 md:py-4 md:px-7 bg-[#FF444A] rounded-lg text-white ml-2"
+          >
             Search
           </button>
         </div>
       </div>
     </div>
   );
+};
+
+Banner.propTypes = {
+  searchCategory: PropTypes.string.isRequired,
+  handleCategoryChange: PropTypes.func.isRequired,
+  handleSearchClick: PropTypes.func.isRequired,
 };
 
 export default Banner;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,13 +1,35 @@
 import { useLoaderData } from "react-router-dom";
 import Donations from "../../components/Donations/Donations";
 import Banner from "../../components/Header/Banner/Banner";
+import { useState } from "react";
 
 const Home = () => {
   const donations = useLoaderData();
+  const [searchCategory, setSearchCategory] = useState("");
+  const [filteredDonations, setFilteredDonations] = useState([]);
+
+  const handleCategoryChange = (event) => {
+    setSearchCategory(event.target.value);
+  };
+  const handleSearchClick = () => {
+    const lowercaseCategory = searchCategory.toLowerCase();
+    const newFilteredDonations = donations.filter((donation) =>
+      donation.category.toLowerCase().includes(lowercaseCategory)
+    );
+    setFilteredDonations(newFilteredDonations);
+    setSearchCategory("");
+  };
+  // console.log(donations);
   return (
     <div>
-      <Banner></Banner>
-      <Donations donations={donations}></Donations>
+      <Banner
+        searchCategory={searchCategory}
+        handleCategoryChange={handleCategoryChange}
+        handleSearchClick={handleSearchClick}
+      ></Banner>
+      <Donations
+        donations={filteredDonations.length ? filteredDonations : donations}
+      ></Donations>
     </div>
   );
 };
